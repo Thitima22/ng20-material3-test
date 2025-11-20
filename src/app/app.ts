9461f614f4c614de2165f8f09e44f742191d7316
Dev-Lemon <gapptech@outlook.com>
2025-11-20 15:20:28 +0700

    refactor: 🍋 config material-color-utilities

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, WritableSignal, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { themeFromSourceColor, hexFromArgb, argbFromHex } from '@material/material-color-utilities';
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatDividerModule } from "@angular/material/divider";
@@ -11,4 +12,24 @@ import { MatDividerModule } from "@angular/material/divider";
   styleUrl: './app.scss',
 })
 export class App {
+  onPrimarySignal: WritableSignal<string> = signal<string>('');
+  onSurfaceSignal: WritableSignal<string> = signal<string>('');
+
+  constructor() {
+    const seedColor = '#feb9cf';
+
+    const seedArgb = argbFromHex(seedColor);
+    const materialTheme = themeFromSourceColor(seedArgb);
+
+    const primaryPalette = materialTheme.palettes.primary;
+    const neutralPalette = materialTheme.palettes.neutral;
+
+    const onPrimary = hexFromArgb(primaryPalette.tone(100));
+    // console.log('onPrimary: ', onPrimary)
+    this.onPrimarySignal.set(onPrimary);
+    
+    const onSurface = hexFromArgb(neutralPalette.tone(10));
+    // console.log('onSurface: ', onSurface)
+    this.onSurfaceSignal.set(onSurface);
+  }
 }
